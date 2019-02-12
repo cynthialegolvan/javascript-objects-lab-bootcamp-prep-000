6d8e9848dc1e25f1581689e043a96118122ca929
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-12 08:49:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var newRecipes = Object.assign({}, object)
-  delete newRecipes.key
+  delete newRecipes[key]
   return newRecipes
 }
