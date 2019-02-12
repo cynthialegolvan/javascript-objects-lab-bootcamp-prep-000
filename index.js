73e054bf98ea439d051885b297ec182bb792741c
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-12 08:34:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {
   flour: "2 cups"
 }
-function updateObjectWithKeyAndVaule(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
